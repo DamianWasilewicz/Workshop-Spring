7df0f4f0d9cecd0ed26fb8976d73d9e748d18b6c
Damian Wasilewicz <33820523+DamianWasilewicz@users.noreply.github.com>
2019-02-04 06:26:47 -0500

    Update dotconn.js

--- 02_canvas/dotconn.js
+++ 02_canvas/dotconn.js
@@ -3,43 +3,47 @@ var ctx = c.getContext("2d")
 var drawn = false;
 var prevX, prevY;
 
-var butt= document.getElementById("clear")
+var butt = document.getElementById("clear")
 
 butt.addEventListener('click', function(e) {
-  if (drawn == true){ // only if has stuff on canvas will it clear
+  if (drawn == true){
+    // only if has stuff on canvas will it clear
   ctx.clearRect(0,0,600,600);
   console.log(e);
   drawn = false;
 }
 else{
-  //prevents default function:wont clear if nothing to clear
+  //prevents default function: wont clear if nothing to clear
   e.preventDefault()
   console.log("Can't clear! Nothing to be cleared!")
 }
 }
 );
 
-// var br = c.getBoundingClientRect()
-// x = br.left
-// y = br.top
+//click: main function to create circle and lines
 c.addEventListener('click', function(e){
   var x = e.offsetX;
   var y = e.offsetY;
   ctx.fillStyle = "green"
   if (drawn == false){
-    //prevents accumulation of pathways (i.e., clicking three spots wont create triangle outline)
+    //prevents accumulation of pathways
+    //(i.e., clicking three spots wont create triangle outline)
     prevX = x
     prevY = y
     drawn = true;
   }
   else{
+    //only draws lines IF previously drawn before
     ctx.beginPath();
     ctx.moveTo(prevX,prevY)
     ctx.lineTo(x,y)
     ctx.stroke();
+    //After drawing line, sets new prevX and prevY for the next
+    //time someone clicks
     prevX = x
     prevY = y
   }
+  //Circle redrawn over line
   ctx.beginPath();
   ctx.ellipse(x,y, 15,15, 0,0, 2 * Math.PI);
   ctx.fill();
