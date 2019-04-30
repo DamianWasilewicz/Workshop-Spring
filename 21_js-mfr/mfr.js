1f10fc603a9f2ccec6e31201dda6ad58609faf80
Damian Wasilewicz <33820523+DamianWasilewicz@users.noreply.github.com>
2019-04-30 07:22:54 -0400

    Update mfr.js

--- 21_js-mfr/mfr.js
+++ 21_js-mfr/mfr.js
@@ -15,15 +15,17 @@ d3.csv("2006_-_2012_School_Demographics_and_Accountability_Snapshot.csv").then(f
     });
 
     console.log(num_enrolled)
-
+    document.getElementById(“num”).innerHTML=num_enrolled;
     var num_third = PS20.map(function(n){
         return parseInt(n["grade3"])
     });
     console.log(num_third)
+    document.getElementById(“numthree”).innerHTML=num_third;
 
     //Get sum of number of third graders for all years of PS20 rows
     var sum_third = num_third.reduce(function(a,b){
         return a+b;
     });
     console.log(sum_third)
+    document.getElementByID(“sumthree”).innerHTML=sum_third;
 });
